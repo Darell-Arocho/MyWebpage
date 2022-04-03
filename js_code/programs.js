78bf951a17a2b79685c853264eadd63e05fc003b
Darell Arocho <darell.arocho@upr.edu>
2022-04-03 16:51:24 -0400

    added functionality to the camry to lambo prob

--- js_code/programs.js
+++ js_code/programs.js
@@ -14,3 +14,29 @@ function printFib(){
     }
     buski.innerHTML = arr;
 }
+
+let temp = "red";
+
+function changeCamry(){
+    let image_src = document.getElementById("changeCamry");
+    let text_src = document.getElementById("lambo_par");
+
+    if(temp == "red"){
+        image_src.src="./images/lambo_huracan.jpg";
+        text_src.innerHTML = "Turn my Lamborghini Huracán into a Toyota Camry!";
+        temp = "green";
+    }
+    else if(temp == "green"){
+        image_src.src = "./images/toyota_camry.jpg";
+        text_src.innerHTML = "Turn my Toyota Camry into a Lamborghini Huracán!"
+        temp = "red";
+    }
+
+    /*if(document.getElementById("changeCamry").src="./images/toyota_camry.jpg"){
+    document.getElementById("changeCamry").src="./images/lambo_huracan.jpg";
+    }
+    else if(document.getElementById("changeCamry").src="./images/lambo_huracan.jpg"){
+        document.getElementById("lambo_par").innerHTML = "Turn my Lamborghini Huracán into a Toyota Camry!";
+        document.getElementById("changeCamry").src="./images/toyota_camry.jpg";
+    }*/
+}
